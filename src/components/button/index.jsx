974598b6d5bf91376d017abd4f76c3915ab1266b
zys <zys@outlook.in>
2023-06-23 16:50:22 +0800

    update button component

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -7,14 +7,14 @@ import './index.css'
 export default memo((props)=>{
     const onClick = useMemoizedFn((e)=>{
         e.stopPropagation();
-        props?.onClick?.(e, props)
+        !props.disabled && props?.onClick?.(e, props)
     });
 
     return (
         <button
             role={props.role}
             data-compact={props.compact || undefined}
-     
+            style={props.style}
             data-disabled={props?.disabled || undefined}
             className={classes('btn-item', props.className) }
             onClick={onClick}
