a4e1f4cf66ef5b77be15d2b0541409d16cc7eefd
zys <zys@outlook.in>
2023-06-24 08:45:16 +0800

    add copy-404 func to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs';
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	 
   	plugins: [
-		react()
+		react(),
+		{
+			name: 'copy-404',
+			async writeBundle() {
+			  await fs.copyFileSync('./docs/index.html', './docs/404.html');
+			}
+		}
 	],
   
 	resolve: {
