903345f023ea29e3e2e1deab3a8f0ad6d2b7e295
zys <zys@outlook.in>
2023-06-25 01:28:38 +0800

    optimize card

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,17 +1,14 @@
-import { memo, useState } from 'react'
- 
-import { useMemoizedFn, useHover } from '@/common/hooks'
-import './index.css' 
+import { memo } from 'react'
+import { classes } from '@/common/common'
+import './index.css'
  
 export default memo((props)=>{
-    const [hoverRef, hovered] = useHover();
- 
     return (
-        <div className='card' style={props.style}> 
-            <div className='card-header' style={props.headerStyle}>
+        <div className={classes('card', props.className)}  style={props.style}> 
+            <div className={classes('card-header', props.headerClassName)} style={props.headerStyle}>
                 {props.title}
             </div>
-            <div className='card-body' style={props.bodyStyle}>
+            <div className={classes('card-body', props.bodyClassName)} style={props.bodyStyle}>
                 {props.children}
             </div> 
         </div>
