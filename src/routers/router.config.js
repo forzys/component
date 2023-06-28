e5edc0d2552da3c07e73cb5445ff37b0ccf629b2
zys <zys@outlook.in>
2023-06-28 19:13:43 +0800

    hidden blog

--- src/routers/router.config.js
+++ src/routers/router.config.js
@@ -4,11 +4,20 @@ const routers = [
         path: "/", 
         component: '@/layout/index.tsx', 
         children:[
-            { 
-                index: true, 
-                component: '@/pages/video/index.tsx', 
-            }, 
+            // { 
+            //     index: true, 
+            //     component: '@/pages/blog/index.tsx', 
+            // }, 
+            // {
+            //     path: "/blog",   
+            //     component: '@/pages/blog/index.tsx',  
+            // }, 
+            // {
+            //     path: "/blog/:id",   
+            //     component: '@/pages/blog/index.tsx',  
+            // }, 
             {
+                index: true, 
                 path: "/video",   
                 component: '@/pages/video/index.tsx',  
             }, 
