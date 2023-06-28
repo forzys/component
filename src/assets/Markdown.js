3167bd5b5c686ba6fa89343c376ec0f2d3df7d96
zys <zys@outlook.in>
2023-06-28 19:12:49 +0800

    Markdown support Table

--- src/assets/Markdown.js
+++ src/assets/Markdown.js
@@ -164,6 +164,7 @@
 
             // Convert all tabs to spaces.
             text = _Detab(text);
+            text = _FormatTable(text);
 
             // Strip any lines consisting only of spaces and tabs.
             // This makes subsequent regexen easier to write, because we can
@@ -434,7 +435,7 @@
             text = _DoItalicsAndBold(text);
 
             // Do hard breaks:
-            text = text.replace(/  +\n/g, " <br>\n");
+            text = text.replace(/  +\n/g, " <br />\n");
 
             return text;
         }
@@ -1321,7 +1322,141 @@
         function escapeCharacters_callback(wholeMatch, m1) {
             var charCodeToEscape = m1.charCodeAt(0);
             return "~E" + charCodeToEscape + "E";
+            
         }
+
+
+        function _FormatTable(text, options = {}){
+            var tableRgx       = /^ {0,3}\|?.+\|.+\n {0,3}\|?[ \t]*:?[ \t]*(?:[-=]){2,}[ \t]*:?[ \t]*\|[ \t]*:?[ \t]*(?:[-=]){2,}[\s\S]+?(?:\n\n|¨0)/gm,
+            //singeColTblRgx = /^ {0,3}\|.+\|\n {0,3}\|[ \t]*:?[ \t]*(?:[-=]){2,}[ \t]*:?[ \t]*\|[ \t]*\n(?: {0,3}\|.+\|\n)+(?:\n\n|¨0)/gm;
+            singeColTblRgx = /^ {0,3}\|.+\|[ \t]*\n {0,3}\|[ \t]*:?[ \t]*(?:[-=]){2,}[ \t]*:?[ \t]*\|[ \t]*\n( {0,3}\|.+\|[ \t]*\n)*(?:\n|¨0)/gm;
+             
+            function parseStyles (sLine) {
+                if (/^:[ \t]*--*$/.test(sLine)) {
+                    return ' style="text-align:left;"';
+                } else if (/^--*[ \t]*:[ \t]*$/.test(sLine)) {
+                    return ' style="text-align:right;"';
+                } else if (/^:[ \t]*--*[ \t]*:$/.test(sLine)) {
+                    return ' style="text-align:center;"';
+                } else {
+                    return '';
+                }
+            }
+
+            function parseHeaders (header, style) {
+                var id = '';
+                header = header.trim();
+                // support both tablesHeaderId and tableHeaderId due to error in documentation so we don't break backwards compatibility
+                if (options.tablesHeaderId || options.tableHeaderId) {
+                  id = ' id="' + header.replace(/ /g, '_').toLowerCase() + '"';
+                }
+                header = _RunSpanGamut(header)
+                return '<th' + id + style + '>' + header + '</th>\n';
+            }
+
+
+            function parseCells (cell, style) {
+                var subText = _RunSpanGamut(cell)
+                return '<td' + style + '>' + subText + '</td>\n';
+            }
+
+            function buildTable (headers, cells) {
+                var tb = '<table>\n<thead>\n<tr>\n',
+                    tblLgn = headers.length;
+            
+                for (var i = 0; i < tblLgn; ++i) {
+                  tb += headers[i];
+                }
+                tb += '</tr>\n</thead>\n<tbody>\n';
+            
+                for (i = 0; i < cells.length; ++i) {
+                  tb += '<tr>\n';
+                  for (var ii = 0; ii < tblLgn; ++ii) {
+                    tb += cells[i][ii];
+                  }
+                  tb += '</tr>\n';
+                }
+                tb += '</tbody>\n</table>\n';
+                return tb;
+            }
+            
+            function parseTable (rawTable) {
+                var i, tableLines = rawTable.split('\n');
+            
+                for (i = 0; i < tableLines.length; ++i) {
+                  // strip wrong first and last column if wrapped tables are used
+                  if (/^ {0,3}\|/.test(tableLines[i])) {
+                    tableLines[i] = tableLines[i].replace(/^ {0,3}\|/, '');
+                  }
+                  if (/\|[ \t]*$/.test(tableLines[i])) {
+                    tableLines[i] = tableLines[i].replace(/\|[ \t]*$/, '');
+                  }
+                  // parse code spans first, but we only support one line code spans
+                  tableLines[i] = _DoCodeSpans(tableLines[i]) 
+                }
+            
+                var rawHeaders = tableLines[0].split('|').map(function (s) { return s.trim();}),
+                    rawStyles = tableLines[1].split('|').map(function (s) { return s.trim();}),
+                    rawCells = [],
+                    headers = [],
+                    styles = [],
+                    cells = [];
+            
+                tableLines.shift();
+                tableLines.shift();
+            
+                for (i = 0; i < tableLines.length; ++i) {
+                  if (tableLines[i].trim() === '') {
+                    continue;
+                  }
+                  rawCells.push(
+                    tableLines[i]
+                      .split('|')
+                      .map(function (s) {
+                        return s.trim();
+                      })
+                  );
+                }
+            
+                if (rawHeaders.length < rawStyles.length) {
+                  return rawTable;
+                }
+            
+                for (i = 0; i < rawStyles.length; ++i) {
+                  styles.push(parseStyles(rawStyles[i]));
+                }
+            
+                for (i = 0; i < rawHeaders.length; ++i) {
+                    if(typeof styles[i] === 'undefined'){
+                        styles[i] = '';
+                    }
+                  
+                  headers.push(parseHeaders(rawHeaders[i], styles[i]));
+                }
+            
+                for (i = 0; i < rawCells.length; ++i) {
+                  var row = [];
+                  for (var ii = 0; ii < headers.length; ++ii) { 
+                    row.push(parseCells(rawCells[i][ii], styles[ii]));
+                  }
+                  cells.push(row);
+                }
+            
+                return buildTable(headers, cells);
+            }
+
+            text = text.replace(/\\(\|)/g, escapeCharacters_callback);
+
+            // parse multi column tables
+            text = text.replace(tableRgx, parseTable);
+
+            // parse one column tables
+            text = text.replace(singeColTblRgx, parseTable);
+ 
+            return text
+
+        }  
+
 
     }; // end of the Markdown.Converter constructor
 
